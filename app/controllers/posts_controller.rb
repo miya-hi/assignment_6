2b2ba6389fe7ca56c82513d0744d4cfd99f8dcaa
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-05-30 11:40:17 +0900

    createメソッド追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path
+      else
+        render :new
+      end
+    end
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
